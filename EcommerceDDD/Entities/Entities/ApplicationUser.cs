db334bfe2beb02608a61ab73202bf5591322afc4
Aparecido789 <aparecidofranciscosilva10@gmail.com>
2020-10-02 08:47:06 -0300

    Criação da Aplicação WEB e Criação do Banco de Dados(SqlServer) com Migration

---
+++ EcommerceDDD/Entities/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
--- EcommerceDDD/infrastructure/Configuration/ContextBase.cs
+++ EcommerceDDD/infrastructure/Configuration/ContextBase.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,7 +7,7 @@ using System.Text;
 
 namespace infrastructure.Configuration
 {
-    public class ContextBase : DbContext
+    public class ContextBase : IdentityDbContext<ApplicationUser>
     {
         public ContextBase(DbContextOptions<ContextBase> options):base(options)
         {
