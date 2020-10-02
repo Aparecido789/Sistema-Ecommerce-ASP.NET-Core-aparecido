48a9d66bd948b08e2e4ccc097c34ead18adda697
Aparecido789 <aparecidofranciscosilva10@gmail.com>
2020-10-02 07:54:29 -0300

    Implementando a Infraestrutura do Projeto

---
+++ EcommerceDDD/infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options):base(options)
+        {
+
+        }
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnetionConfig());
+                base.OnConfiguring(optionsBuilder);
+
+            }
+        }
+
+        private string GetStringConnetionConfig()
+        {
+            string strCon = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            return strCon;
+        }
+    }
+}
---
+++ EcommerceDDD/infrastructure/Repository/Repositories/RepositoryProduct.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces.InterfaceProduct;
+using Entities.Entities;
+using infrastructure.Repository.Generics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace infrastructure.Repository.Repositories
+{
+    public class RepositoryProduct : RepositoryGenerics<Produto>, IProduct
+    {
+    }
+}
